21a6feea708f1bc7cf681da2529597597622816e
a.boikov <alex8oikov@yandex.ru>
2025-09-13 10:50:46 +0300

    определение символов для сравнения сделано в рамках цикла перебора чисел от 0 до 9 включительно

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -19,24 +19,18 @@ namespace FirstConsoleApp
             {
                 char symbol = input[index];
 
-                if(symbol == '0' || symbol == '1'
-                     || symbol == '2'
-                      || symbol == '3'
-                       || symbol == '4'
-                        || symbol == '5'
-                         || symbol == '6'
-                          || symbol == '7'
-                           || symbol == '8'
-                            || symbol == '9'   
-                            || symbol == ' '
-                            )
+                for(int n = 0; n <= 9; ++n)
                 {
-                    isNumber = true;
-                }
-                else
-                {
-                    isNumber = false;
-                    index = input.Length;
+                    char numberSymbol = Convert.ToChar(n);
+                    if(symbol == numberSymbol || symbol == ' ')
+                    {
+                        isNumber = true;
+                    }
+                    else
+                    {
+                        isNumber = false;
+                        index = input.Length;
+                    }
                 }
             }
 
