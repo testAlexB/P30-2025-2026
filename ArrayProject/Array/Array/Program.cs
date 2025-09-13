f21baffc484b9cfd3deca142efbf943e44b59720
a.boikov <alex8oikov@yandex.ru>
2025-09-13 11:46:33 +0300

    тестовый проект для работы с массивами

---
+++ ArrayProject/Array/Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Array
+{
+    public class Program
+    {
+        static void Main()
+        {
+            /// создание челочисленного массива с 5 элементами
+            int[] array = new int[5]; /// все элементы по умолчанию будут = 0
+
+            /// вывод элементов массива
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[4]);
+
+            Console.WriteLine("Размер массива = " + array.Length);
+
+            /// перебор всех элементов массива
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.WriteLine($"Element{index} = {array[index]}");
+            }
+        }
+    }
+}
