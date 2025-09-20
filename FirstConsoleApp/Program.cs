7ee2889f6412ac8dbe7f42e345d720d238d0ab02
a.boikov <alex8oikov@yandex.ru>
2025-09-20 11:02:19 +0300

    реализация проверки вводимых символов на число, используя символьный массив допустимых знаков

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace FirstConsoleApp
 {
@@ -15,14 +16,13 @@ namespace FirstConsoleApp
 
             bool isNumber = true;
 
+            char[] numberSymbols = new char[11] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ' };
+           //bool isG = numberSymbols.Contains('g'); /// определяет, находится ли символ g в массиве numberSymbols
+
             for (int index = 0; index < input.Length; index++)
             {
                 char symbol = input[index];
-
-                for(int n = 0; n <= 9; ++n)
-                {
-                    char numberSymbol = Convert.ToChar(n);
-                    if(symbol == numberSymbol || symbol == ' ')
+                    if(numberSymbols.Contains(symbol))
                     {
                         isNumber = true;
                     }
@@ -31,7 +31,6 @@ namespace FirstConsoleApp
                         isNumber = false;
                         index = input.Length;
                     }
-                }
             }
 
             if (!isNumber)
