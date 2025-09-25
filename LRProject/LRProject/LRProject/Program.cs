46ccd9a34f0ff5212d3182e1a30a82f1c5221f1c
a.boikov <alex8oikov@yandex.ru>
2025-09-25 10:17:43 +0300

    заполнили массив 3000 случайных чисел в диапазоне [10; 9999]

--- LRProject/LRProject/LRProject/Program.cs
+++ LRProject/LRProject/LRProject/Program.cs
@@ -7,34 +7,12 @@ namespace LRProject
     {
         static void Main()
         {
-            int[] array = new int[1990];
-            int a = 10;
+            int[] array = new int[3000];
+
+            Random rnd = new Random();
             for(int i = 0; i < array.Length; ++i)
             {
-                array[i] = a;
-                ++a;
-            }
-
-            List<int> result = new List<int>();
-            foreach(int element in array)
-            {
-                int countD = 0;
-                for(int d = 2; d < element; ++d)
-                {
-                    /// делится ли число нацело на d
-                    if(element % d == 0)
-                    {
-                        ++countD;
-                        break;
-                    }
-                }
-
-                /// если делителей нет
-                if(countD == 0)
-                {
-                    /// то число простое
-                    result.Add(element);
-                }
+                array[i] = rnd.Next(10, 10000);
             }
         }
     }
