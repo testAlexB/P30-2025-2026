d2a232908e175cd1b35c987e233382e6561b0893
a.boikov <alex8oikov@yandex.ru>
2025-09-25 09:53:23 +0300

    реализован ввод с клавиатуры размера массива

---
+++ LRProject/LRProject/LRProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LRProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 0;
+
+            Console.Write("Число элементов массива = ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
