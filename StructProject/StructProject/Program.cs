08f83c74b448b08f37e2f5117dbbb191753a710e
a.boikov <alex8oikov@yandex.ru>
2025-11-08 11:13:00 +0300

    произвели расчет длины вектора по координатам начала и конца

--- StructProject/StructProject/Program.cs
+++ StructProject/StructProject/Program.cs
@@ -7,17 +7,25 @@ namespace StructProject
         static void Main()
         {
             Point beginVector = new Point();
-            beginVector.X = -10;
-            beginVector.Y = 5;
+            beginVector.X = 0;
+            beginVector.Y = 0;
             beginVector.Name = "Начало вектора";
 
             Point endVector = new Point();
-            endVector.X = 10;
-            endVector.Y = -5;
+            endVector.X = 0;
+            endVector.Y = 55;
             endVector.Name = "Конец вектора";
 
             Console.WriteLine($"{beginVector.Name}:({beginVector.X}, {beginVector.Y})" +
                               $" | {endVector.Name}:({endVector.X}, {endVector.Y})");
+
+            int diffX = (int)Math.Pow(endVector.X - beginVector.X, 2);
+            int diffY = (int)Math.Pow(endVector.Y - beginVector.Y, 2);
+
+            int diff = diffX + diffY;
+
+            double lenVector = Math.Sqrt(diff);
+            Console.WriteLine("Длина вектора = " + lenVector);
         }
     }
 }
