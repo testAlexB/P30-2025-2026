71c4b5942894bad460408ef209e2f672bdaeb0c9
a.boikov <alex8oikov@yandex.ru>
2025-11-08 10:56:48 +0300

    создали объект типа структуры Point, характекриющий точку начала вектора

--- StructProject/StructProject/Program.cs
+++ StructProject/StructProject/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main()
         {
+            Point beginVector = new Point();
+            beginVector.X = -10;
+            beginVector.Y = 5;
+            beginVector.Name = "Начало вектора";
         }
     }
 }
